e957a041e01c4d91ab2726a79ac22d08f05db582
olimejj <olimejj@gmail.com>
2023-12-30 07:25:36 -0700

    the tutorial has begun

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-
-
-using namespace std;
-
-int main(){
-    cout << "Hello Word\n";
-}
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "graphics.h"
+
+
+using namespace std;
+
+int main(){
+    cout << "Hello Word\n";
+    Graphics graphics;
+    for(int x = 0; x < 10000; x++){
+        cout << "running still\n";
+    }
+
+
+    return 0;
+}
